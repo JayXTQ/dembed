c57c956dc990c68a1637a5bbc3fff56d70b8928a
Jay <me@jayxtq.xyz>
2024-05-01 11:48:06 +0100

    feat: add more stuff in attempt to fix embed

--- src/createEmbed.ts
+++ src/createEmbed.ts
@@ -1,90 +1,131 @@
-export default (url: string, src: string, type: 'image' | 'video', description: string, resolution?: { w: number; h: number }) => {
-    type Metas = Array<{name: string; content: string} | { property: string; content: string }>
+export default (
+    url: string,
+    src: string,
+    type: "image" | "video",
+    description: string,
+    resolution?: { w: number; h: number },
+) => {
+    type Metas = Array<
+        | { name: string; content: string }
+        | { property: string; content: string }
+    >;
     const metas: Metas = [
         {
-            name: 'og:title',
-            content: 'dembed'
+            property: "og:title",
+            content: "dembed",
         },
         {
-            name: 'og:url',
-            content: url
+            property: "og:url",
+            content: url,
         },
         {
-            name: 'twitter:url',
-            content: url
+            name: "twitter:url",
+            content: url,
         },
         {
-            name: 'twitter:title',
-            content: 'dembed'
+            name: "twitter:title",
+            content: "dembed",
         },
         {
-            name: 'og:description',
-            content: description
+            property: "og:description",
+            content: description,
         },
         {
-            name: 'twitter:description',
-            content: description
+            property: "twitter:description",
+            content: description,
         },
-    ]
+        {
+            property: "og:site_name",
+            content: "dembed",
+        },
+    ];
     const imageMetas: Metas = [
         {
-            name: 'twitter:card',
-            content: 'summary_large_image'
+            name: "twitter:card",
+            content: "summary_large_image",
         },
         {
-            name: 'twitter:image',
-            content: src
-        }
-    ]
+            name: "twitter:image",
+            content: src,
+        },
+    ];
     const videoMetas: Metas = [
         {
-            name: 'twitter:card',
-            content: 'player'
+            name: "twitter:card",
+            content: "player",
         },
         {
-            name: 'twitter:player:width',
-            content: resolution?.w.toString() as string
+            name: "twitter:player:width",
+            content: resolution?.w.toString() as string,
         },
         {
-            name: 'twitter:player:height',
-            content: resolution?.h.toString() as string
+            name: "twitter:player:height",
+            content: resolution?.h.toString() as string,
         },
         {
-            name: 'twitter:player:stream',
-            content: src
+            name: "twitter:player:stream",
+            content: src,
         },
         {
-            name: 'twitter:player:stream:content_type',
-            content: 'video/mp4'
+            name: "twitter:player:stream:content_type",
+            content: "video/mp4",
         },
         {
-            name: 'og:video',
-            content: src
+            property: "og:video",
+            content: src,
         },
         {
-            name: 'og:video:secure_url',
-            content: src
+            property: "og:video:secure_url",
+            content: src,
         },
         {
-            name: 'og:video:type',
-            content: 'video/mp4'
+            property: "og:video:type",
+            content: "video/mp4",
         },
         {
-            name: 'og:video:width',
-            content: resolution?.w.toString() as string
+            property: "og:video:width",
+            content: resolution?.w.toString() as string,
         },
         {
-            name: 'og:video:height',
-            content: resolution?.h.toString() as string
+            property: "og:video:height",
+            content: resolution?.h.toString() as string,
         },
-    ]
-
+    ];
 
     return `<!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        ${metas.map(meta => `<meta ${Object.keys(meta).map(key => `${key}="${meta[key]}"`).join(' ')} />`).join('\n')}
-        ${type === 'image' ? imageMetas.map(meta => `<meta ${Object.keys(meta).map(key => `${key}="${meta[key]}"`).join(' ')} />`).join('\n') : videoMetas.map(meta => `<meta ${Object.keys(meta).map(key => `${key}="${meta[key]}"`).join(' ')} />`).join('\n')}
+        <meta charset="utf-8" />
+        ${metas
+            .map(
+                (meta) =>
+                    `<meta ${Object.keys(meta)
+                        .map((key) => `${key}="${meta[key]}"`)
+                        .join(" ")} />`,
+            )
+            .join("\n")}
+        ${
+            type === "image"
+                ? imageMetas
+                      .map(
+                          (meta) =>
+                              `<meta ${Object.keys(meta)
+                                  .map((key) => `${key}="${meta[key]}"`)
+                                  .join(" ")} />`,
+                      )
+                      .join("\n")
+                : videoMetas
+                      .map(
+                          (meta) =>
+                              `<meta ${Object.keys(meta)
+                                  .map((key) => `${key}="${meta[key]}"`)
+                                  .join(" ")} />`,
+                      )
+                      .join("\n")
+        }
     </head>
-</html>`
-}
+    <body>
+        why u peekin?
+    </body>
+</html>`;
+};
