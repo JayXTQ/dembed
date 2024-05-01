0d7ace80d1f6ecfaf3a0fad8fb92552c845d92c1
Jay <me@jayxtq.xyz>
2024-05-01 23:31:31 +0100

    feat: need for tiktok redirect

--- src/providers/tiktok.ts
+++ src/providers/tiktok.ts
@@ -9,10 +9,6 @@ export default async (
     const page = await browser.newPage();
     await page.goto(url);
     await page.setViewport({ width: 1920, height: 1080 });
-    let resolution: { w: number; h: number } | undefined = undefined;
-    const video = await page.waitForSelector("video").catch(() => null);
-    if (!video) return null;
-    const src = await page.evaluate((video) => video.src, video);
     const username = await page.$$('span[data-e2e="browser-nickname"] > span')
     if (!username) return null;
     let user = "";
@@ -26,13 +22,27 @@ export default async (
     let description: ElementHandle | string | null = await page.$('h1[data-e2e="browse-video-desc"]')
     if (!description) description = 'No description';
     if(typeof description !== 'string' ) description = extractText((await description.getProperty('innerHTML')).toString().replace('JSHandle:', ''));
-    console.log(src)
+    url = page.url();
+    await page.close();
     const embed = createEmbed(
         url,
-        src,
+        '/video/tiktok/' + url.split('https://www.tiktok.com/')[1],
         "video",
-        `Post by ${user}: ${description}`,
-        resolution
+        `Post by ${user}: ${description}`
     );
     return embed;
+};
+
+export const video = async (
+    browser: Browser,
+    data: string,
+): Promise<string | null> => {
+    const page = await browser.newPage();
+    await page.goto(`https://www.tiktok.com/${data}`);
+    await page.setViewport({ width: 1920, height: 1080 });
+    const video = await page.waitForSelector("video").catch(() => null);
+    if (!video) return null;
+    const src = await page.evaluate((video) => video.src, video);
+    await page.close();
+    return src;
 };
