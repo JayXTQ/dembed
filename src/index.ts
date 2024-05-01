78a79ca2ccfc19eec6625e3cb669d970b382598c
Jay <me@jayxtq.xyz>
2024-05-01 13:23:16 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -35,8 +35,8 @@ app.get("/http*", async (req: Request, res: Response) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     const url = req.url.slice(1);
     if (
-        req.header["user-agent"] !==
-        "Mozilla/5.0 (compatible; Discordbot/2.0; +discordapp.com)"
+        !req.header["user-agent"]?.includes("Discordbot")
+        // "Mozilla/5.0 (compatible; Discordbot/2.0; +discordapp.com)"
     )
         res.redirect(url);
     if (url.length <= 4) return;
